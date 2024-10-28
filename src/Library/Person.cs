bfb06432498666990844fdddd3a54e44c7013091
Pilar Antelo <pilarantelo.2705@gmail.com>
2024-10-28 10:06:44 -0300

    PersonClass

---
+++ src/Library/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace Library;
+
+public class Person
+{
+    private string Name { get; set; }
+    
+    private int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+    
+    
+}
