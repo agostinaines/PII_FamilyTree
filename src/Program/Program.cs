0aec24d1ecf64a6cbee46fec2fe4e6547fa7b6fb
Pilar Antelo <pilarantelo.2705@gmail.com>
2024-10-29 10:29:44 -0300

    Update Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,17 +6,15 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Person Ana = new Person("Ana Martinez", 39);
-            Person Martina = new Person("Martina Caetano", 18);
-
-            Node<Person> raiz = new Node<Person>(Ana);
-            Node<Person> nosequeponeraca = new Node<Person>(Martina);
-
+            
             Person Marimarta = new Person("Marimarta", 86);
             Person Dorothy = new Person("Dorothy", 54);
 
             Person Ines = new Person("Inés", 18);
             Person Titi = new Person("Titi", 3);
+            
+            Person Ana = new Person("Ana Martinez", 39);
+            Person Martina = new Person("Martina Caetano", 18);
             
             
             Node<Person> n1 = new Node<Person>(Marimarta);
@@ -25,8 +23,13 @@ namespace Program
             Node<Person> n3 = new Node<Person>(Ines);
             Node<Person> n4 = new Node<Person>(Titi);
             
+            Node<Person> n5 = new Node<Person>(Ana);
+            Node<Person> n6 = new Node<Person>(Martina);
+            
             n1.AddChildren(n2);
             n3.AddChildren(n4);
+            n1.AddChildren(n5);
+            n2.AddChildren(n6);
         }
     }
 }
