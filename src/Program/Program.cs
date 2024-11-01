9619c91a044399d658b25b4ea286fec73e9f2839
Romina-cantoni <romina.cantoni@correo.ucu.edu.uy>
2024-11-01 13:11:56 -0300

    Update Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -9,34 +9,38 @@ namespace Program
         static void Main(string[] args)
         {
             
-            Person Marimarta = new Person("Marimarta", 86);
-            Person Dorothy = new Person("Dorothy", 54);
+            Person marimarta = new Person("Marimarta", 86);
+            Person dorothy = new Person("Dorothy", 54);
 
-            Person Lourdes = new Person("Lourdes", 50);
-            Person Ines = new Person("Inés", 18);
-            Person Titi = new Person("Titi", 3);
+            Person lourdes = new Person("Lourdes", 50);
+            Person ines = new Person("Inés", 18);
+            Person titi = new Person("Titi", 3);
             
-            Person Ana = new Person("Ana Martinez", 39);
-            Person Martina = new Person("Martina Caetano", 18);
-            Person Teddy = new Person("Teddy", 2);
+            Person ana = new Person("Ana Martinez", 39);
+            Person martina = new Person("Martina Caetano", 18);
+            Person teddy = new Person("Teddy", 2);
+
+            Person alicia = new Person("Alicia", 79);
+            Person enrica = new Person("Enrica", 47);
+            Person guillermo = new Person("Guillermo", 11);
             
             
-            Node<Person> n1 = new Node<Person>(Marimarta);
-            Node<Person> n2 = new Node<Person>(Dorothy);
+            Node<Person> n1 = new Node<Person>(marimarta);
+            Node<Person> n2 = new Node<Person>(dorothy);
             // Node<Person> n3 = new Node<Person>()
 
-            Node<Person> n4 = new Node<Person>(Lourdes);
-            Node<Person> n5 = new Node<Person>(Ines);
-            Node<Person> n6 = new Node<Person>(Titi);
+            Node<Person> n4 = new Node<Person>(lourdes);
+            Node<Person> n5 = new Node<Person>(ines);
+            Node<Person> n6 = new Node<Person>(titi);
             
-            Node<Person> n7 = new Node<Person>(Ana);
-            Node<Person> n8 = new Node<Person>(Martina);
-            Node<Person> n9 = new Node<Person>(Teddy);
+            Node<Person> n7 = new Node<Person>(ana);
+            Node<Person> n8 = new Node<Person>(martina);
+            Node<Person> n9 = new Node<Person>(teddy);
             
             //Personas de romi
-            // Node<Person> n10 = new Node<Person>()
-            // Node<Person> n11 = new Node<Person>()
-            // Node<Person> n12 = new Node<Person>()
+            Node<Person> n10 = new Node<Person>(alicia);
+            Node<Person> n11 = new Node<Person>(enrica);
+            Node<Person> n12 = new Node<Person>(guillermo);
             
             n1.AddChildren(n2);
             n1.AddChildren(n7);
@@ -45,6 +49,9 @@ namespace Program
             n4.AddChildren(n5);
             n5.AddChildren(n6);
             n8.AddChildren(n9);
+            n10.AddChildren(n11);
+            n11.AddChildren(n12);
+            
 
             PersonVisitor personVisitor = new PersonVisitor();
             
